610b393c9479a689809b49345cf237fb0ec1517b
Facundo Gayoso <facusbrr@gmail.com>
2025-12-10 01:23:19 -0300

    feat(nvim): update plugins and config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,19 @@
--- Carga config y arranca Lazy
+-- Load config and initialize Lazy
 require("config.lazy")
-require("config.obsidian_maps").setup_autocmd()
 
+-- Optional modules with error handling
+pcall(function()
+  require("config.obsidian_maps").setup_autocmd()
+end)
+
+pcall(function()
+  require("config.keymaps")
+end)
+
+pcall(function()
+  require("config.dap")
+end)
+
+-- Timeout configurations
 vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
